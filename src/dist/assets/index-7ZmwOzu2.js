e93231f1fc3d5c5c3d50d6ab54d8552ce0765acd
Vintaragroup <ryan@vintaragroup.com>
2025-10-07 17:26:15 -0400

    chore: ignore build artifacts (src/dist)

--- src/dist/assets/index-7ZmwOzu2.js
+++
@@ -1,6 +0,0 @@
-import{c as s,r as o}from"./index-f6AKZqSw.js";/**
- * @license lucide-react v0.544.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M12 15V3",key:"m9g1x1"}],["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["path",{d:"m7 10 5 5 5-5",key:"brsn70"}]],u=s("download",a);function d(t){const e=o.useRef(t);return o.useEffect(()=>{e.current=t}),o.useMemo(()=>(...r)=>{var n;return(n=e.current)==null?void 0:n.call(e,...r)},[])}export{u as D,d as u};
